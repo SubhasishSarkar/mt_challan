40ceca64c4d1dbe9dc2d0987e9037c9f5c8f0b7b
Subhasish Sarkar <113631857+SarkarSubhasish@users.noreply.github.com>
2023-05-20 00:44:18 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
+import { useRef } from 'react';
 import './App.css';
+import ReactPrint from 'react-to-print'
+import Challan from './components/Challan';
 
 function App() {
+  const ref = useRef()
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main >
+        <div ref={ref}>
+          <Challan />
+          <div className='border-2 border-white border-dashed border-b-black'></div>
+          <Challan />
+        </div>
+        <div className='flex justify-center'>
+          <ReactPrint
+            trigger={() => <button className='border-2 border-blue-500 bg-blue-400 text-white  font-bold py-2  px-4 rounded'>Print</button>}
+            content={() => ref.current}
+          />
+        </div>
+        
+      </main>
     </div>
   );
 }
